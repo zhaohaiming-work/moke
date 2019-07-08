ce844082a25e8dd58fd45211194fa50b770326d9
haiming <1072950174@qq.com>
2019-07-08 10:25:35 +0800

    提交合同的数据

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -16,5 +16,30 @@ module.exports = [
     fsUrl: `projectSearch`,
     methond: 'get',
     reqUrl: '/asset2/api/project/get'
-  }
+  },
+  // 运营管理 - 创建合同
+  {
+    fsUrl: `contract-save`,
+    methond: 'post',
+    reqUrl: '/api/om/contract/save'
+  },
+  // 运营管理 - 根据条件搜索合同-根据时间显示合同到期合同
+  {
+    fsUrl: `contract-list`,
+    methond: 'get',
+    reqUrl: '/api/om/contract/list'
+  },
+  // 运营管理 - 根据时间显示合同到期监控图
+  {
+    fsUrl: `contract-chart`,
+    methond: 'get',
+    reqUrl: '/api/om/contract/chart'
+  },
+  // 运营管理 - 查看合同详情
+  {
+    fsUrl: `contract-detail`,
+    methond: 'get',
+    reqUrl: '/api/om/contract/detail'
+  },
+
 ]
